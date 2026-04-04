9f498c9117a4c0a2bc05710066b9b5f847b89281
ry727777 <ry727777@gmail.com>
2026-04-04 10:37:04 +0530

    Fixed removed items

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -3,6 +3,9 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import dev.failsafe.internal.util.Assert;
+
 import java.util.List;
 
 public class CartPage {
@@ -18,6 +21,8 @@ public class CartPage {
     private By cartLink = By.className("shopping_cart_link");
     private By invetoryList = By.className("inventory_item");
     private By cartPageItems = By.xpath("//span[@class=\"shopping_cart_badge\"]");
+    private By addBtn = By.xpath(".//button[text()=\"Add to cart\"]");
+    private By removebtn = By.xpath(".//button[text()=\"Remove\"]");
 
     public boolean checkItemsPresent(WebDriver driver){
         itemList = driver.findElements(invetoryList);
@@ -25,18 +30,39 @@ public class CartPage {
         return itemList.size() != 0;
     }
 
+    public int  numberOfElementsCart(WebDriver driver){
+        return Integer.parseInt(driver.findElement(cartPageItems).getText());
+    }
+
     public boolean addToCart(WebDriver driver){
         boolean items = checkItemsPresent(driver);
+        if(!items) return false;
         // add one item to the cart
 
         for(int i=0; i<3; i++){
-            WebElement addToCartBtn = itemList.get(i).findElement(By.tagName("button"));
+            WebElement addToCartBtn = itemList.get(i).findElement(addBtn);
              addToCartBtn.click();
         }
 
-        int total_items = Integer.parseInt(driver.findElement(cartPageItems).getText());
+        int total_items = numberOfElementsCart(driver);
 
         return total_items == 3;
         
+    }
+
+    public boolean removeItems(WebDriver driver){
+
+        int beforeRemoved = numberOfElementsCart(driver);
+
+        // remove one
+        itemList.get(0).findElement(removebtn).click();
+
+        int afterRemovedOne = numberOfElementsCart(driver);
+
+        itemList.get(1).findElement(removebtn).click();
+
+        int afterRemovedSecond = numberOfElementsCart(driver);
+        
+        return beforeRemoved == 3 && afterRemovedOne == 2 && afterRemovedSecond == 1;
     }
 }
---
+++ src/test/java/tests/RemoveItemsTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import utils.ConfigReader;
+import pages.LoginPage;
+import pages.CartPage;
+
+public class RemoveItemsTest extends BaseTest {
+    // login
+    // add items to the cart page
+    // Remove item from  cart page
+
+     @Test(groups = "remove_from_cart")
+    public void removeFromCartTest(){
+        LoginPage loginpage = new LoginPage(driver);
+
+        String user_name = ConfigReader.getProperty("username");
+        String password = ConfigReader.getProperty("password");
+
+        loginpage.login(user_name, password);
+
+        // Add element to the cart page
+        CartPage cart_page = new CartPage(driver);
+        boolean result = cart_page.addToCart(driver);
+        Assert.assertTrue(result, "Add to cart failed");
+
+
+        // remove element
+        boolean result2 = cart_page.removeItems(driver);
+        Assert.assertTrue(result2, "Items not removed from cart page");
+
+    }
+}
